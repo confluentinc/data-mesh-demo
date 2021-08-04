bc2acc52a311af7ac6e243f721ace7a029d92f74
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-08-04 14:08:38 -0500

    added get data product route and config file

--- src/main/java/io/confluent/demo/datamesh/DataProductsController.java
+++ src/main/java/io/confluent/demo/datamesh/DataProductsController.java
@@ -1,18 +1,28 @@
 package io.confluent.demo.datamesh;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
 @RestController
+@RequestMapping("/data-products")
 public class DataProductsController {
 
-    @GetMapping("/data-products")
+    @GetMapping
     public DataProduct[] getDataProducts() {
+        // TODO: Load proper data product list from CC API
         return new DataProduct[] {
             new DataProduct("pageviews", "lsrc-78xpp:.:pageviews-value:1", Collections.emptyList()),
             new DataProduct("users", "lsrc-78xpp:.:users-value:1", Collections.emptyList())
         };
+    }
+
+    @RequestMapping("/{qualifiedName}")
+    public DataProduct getDataProduct(@PathVariable("qualifiedName") String qualifiedName) {
+        // TODO: Load proper data product from parameter
+        return new DataProduct("users", "lsrc-78xpp:.:users-value:1", Collections.emptyList());
     }
 }
