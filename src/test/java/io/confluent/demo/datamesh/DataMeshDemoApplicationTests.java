c3a35d66d8cca6831318b9b586cb53bb84419533
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-08-04 12:28:51 -0500

    init commit

---
+++ src/test/java/io/confluent/demo/datamesh/DataMeshDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.demo.datamesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMeshDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
