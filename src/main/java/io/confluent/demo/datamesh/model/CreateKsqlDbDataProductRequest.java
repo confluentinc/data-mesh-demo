c4d538eec251be76d57381026dabd1758ba85c9b
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-08-16 14:31:37 -0500

    WIP: creating data products

--- src/main/java/io/confluent/demo/datamesh/model/CreateKsqlDbDataProductRequest.java
+++ src/main/java/io/confluent/demo/datamesh/model/CreateKsqlDbDataProductRequest.java
@@ -2,11 +2,14 @@ package io.confluent.demo.datamesh.model;
 
 public class CreateKsqlDbDataProductRequest extends CreateDataProductRequest {
     private final String command;
-    public CreateKsqlDbDataProductRequest(String name, String command) {
+    private final String dataProductName;
+    public CreateKsqlDbDataProductRequest(String name, String command, String dataProductName) {
         setName(name);
         this.command = command;
+        this.dataProductName = dataProductName;
     }
     public String getCommand() {
         return this.command;
     }
+    public String getDataProductName() {return this.dataProductName;}
 }
