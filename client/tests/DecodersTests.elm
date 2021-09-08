7850caa9a5520f940e44ba3c1ac39e132b275075
Kris Jenkins <kjenkins@confluent.io>
2021-09-08 09:49:03 +0100

    UI: Refactoring the decoder tests.

--- client/tests/DecodersTests.elm
+++ client/tests/DecodersTests.elm
@@ -16,19 +16,47 @@ suite =
     describe "Decoders"
         [ describe
             "Decode Streams"
-            [ test "sample response 1 - no error" <|
+            [ test "publish response - correct result" <|
                 \_ ->
-                    decodesSuccessfully decodeStreams dataProductsResponse1
-            , test "sample response 2 - no error" <|
-                \_ ->
-                    decodesSuccessfully decodeStreams dataProductsResponse2
-            , test "sample response 3 - no error" <|
-                \_ ->
-                    decodesSuccessfully decodeStream publishDataProductResponse1
-            , test "sample response 2 - right result" <|
+                    decodesTo decodeStream
+                        publishDataProductResponse1
+                        (StreamDataProduct
+                            { description = "website users"
+                            , name = "users"
+                            , owner = "ybyzek"
+                            , qualifiedName = QualifiedName "lsrc-odr89:.:users-value:1"
+                            , urls =
+                                { lineageUrl =
+                                    { fragment = Nothing
+                                    , host = "confluent.cloud"
+                                    , path = "/environments/env-jn132/clusters/lkc-17pzv/stream-lineage/view/users-value"
+                                    , port_ = Nothing
+                                    , protocol = Https
+                                    , query = Nothing
+                                    }
+                                , portUrl =
+                                    { fragment = Nothing
+                                    , host = "confluent.cloud"
+                                    , path = "/environments/env-jn132/clusters/lkc-17pzv/topics/users"
+                                    , port_ = Nothing
+                                    , protocol = Https
+                                    , query = Nothing
+                                    }
+                                , schemaUrl =
+                                    { fragment = Nothing
+                                    , host = "confluent.cloud"
+                                    , path = "/environments/env-jn132/schema-registry/schemas/users-value"
+                                    , port_ = Nothing
+                                    , protocol = Https
+                                    , query = Nothing
+                                    }
+                                }
+                            }
+                        )
+            , test "streams response - correct result" <|
                 \_ ->
                     decodesTo decodeStreams
-                        dataProductsResponse2
+                        dataProductsResponse1
                         [ StreamDataProduct
                             { description = "website users"
                             , name = "users"
@@ -104,38 +132,6 @@ suite =
 
 dataProductsResponse1 : String
 dataProductsResponse1 =
-    """
-[
-  {
-    "@type": "DataProduct",
-    "name": "users",
-    "qualifiedName": "lsrc-wqg5g:.:users-value:1",
-    "description": "website users",
-    "owner": "rick",
-    "urls": {
-      "schemaUrl": "https://confluent.cloud/environments/env-dxy51/schema-registry/schemas/users-value",
-      "portUrl": "https://confluent.cloud/environments/env-dxy51/clusters/lkc-ypvmp/topics/users",
-      "lineageUrl": "https://confluent.cloud/environments/env-dxy51/clusters/lkc-ypvmp/stream-lineage/view/users-value"
-    }
-  },
-  {
-    "@type": "DataProduct",
-    "name": "pageviews",
-    "qualifiedName": "lsrc-wqg5g:.:pageviews-value:1",
-    "description": "website pageviews",
-    "owner": "adam",
-    "urls": {
-      "schemaUrl": "https://confluent.cloud/environments/env-dxy51/schema-registry/schemas/pageviews-value",
-      "portUrl": "https://confluent.cloud/environments/env-dxy51/clusters/lkc-ypvmp/topics/pageviews",
-      "lineageUrl": "https://confluent.cloud/environments/env-dxy51/clusters/lkc-ypvmp/stream-lineage/view/pageviews-value"
-    }
-  }
-]
-"""
-
-
-dataProductsResponse2 : String
-dataProductsResponse2 =
     """
 [
   {
