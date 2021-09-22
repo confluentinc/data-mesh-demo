00fb212963d64d65eed27060fb0f811499762055
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-09-22 11:39:05 -0500

    Adds a ksqlDB url to the use cases route

--- src/main/java/io/confluent/demo/datamesh/model/UseCase.java
+++ src/main/java/io/confluent/demo/datamesh/model/UseCase.java
@@ -6,18 +6,21 @@ public class UseCase {
     private final String inputs;
     private final String ksqlDbCommand;
     private final String outputTopic;
+    private final String ksqlDbLaunchUrl;
 
     public UseCase(String description, String name, String inputs,
-                   String ksqlDbCommand, String outputTopic) {
+                   String ksqlDbCommand, String outputTopic, String ksqlDbLaunchUrl) {
         this.description = description;
         this.name = name;
         this.inputs = inputs;
         this.ksqlDbCommand = ksqlDbCommand;
         this.outputTopic = outputTopic;
+        this.ksqlDbLaunchUrl = ksqlDbLaunchUrl;
     }
     public String getDescription() {return this.description;}
     public String getName() {return this.name;}
     public String getInputs() {return this.inputs;}
     public String getKsqlDbCommand() {return this.ksqlDbCommand;}
     public String getOutputTopic() {return this.outputTopic;}
+    public String getKsqlDbLaunchUrl() {return this.ksqlDbLaunchUrl;}
 }
