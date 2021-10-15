4cdab5007c0dfb947049984278d4a6c9c05d9c34
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-10-14 20:53:06 -0500

    adds stock trades datagen and data product
    extends wait time because of observed errors during connector provisioning
    related to credential progpation

--- scripts/statements-cloud.sql
+++ scripts/statements-cloud.sql
@@ -1,2 +1,3 @@
+CREATE STREAM stocktrades WITH (kafka_topic='stocktrades', value_format='AVRO');
 CREATE STREAM pageviews WITH (kafka_topic='pageviews', value_format='AVRO');
 CREATE TABLE users (id STRING PRIMARY KEY) WITH (kafka_topic='users', value_format='AVRO');
