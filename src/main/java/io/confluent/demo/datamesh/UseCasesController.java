9460656adf888fe5049eaf5ab3fe149bc01db5da
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-10-18 10:03:35 -0500

    Adds title field to use cases

--- src/main/java/io/confluent/demo/datamesh/UseCasesController.java
+++ src/main/java/io/confluent/demo/datamesh/UseCasesController.java
@@ -26,7 +26,7 @@ public class UseCasesController {
          */
         String cmd = "CREATE STREAM IF NOT EXISTS PAGEVIEWS_ENRICHED\n    with (kafka_topic='pageviews_enriched')\n    AS SELECT U.ID AS USERID, U.REGIONID AS REGION,\n        U.GENDER AS GENDER, V.PAGEID AS PAGE\n    FROM PAGEVIEWS V INNER JOIN USERS U \n    ON V.USERID = U.ID;";
         return new UseCase(
-                "Enrich an event stream",
+                "description",
                 "pageviews_enriched",
                 "pageviews,users",
                 cmd,
@@ -35,12 +35,13 @@ public class UseCasesController {
                         urlService.getKsqlDbUrl(), // &ksqlClusterId=lksqlc-1v2p6&properties=%7B%22auto.offset.reset%22%3A%22latest%22%7D
                         UriUtils.encode(cmd, StandardCharsets.UTF_8.name()),
                         UriUtils.encode(urlService.getKsqlDbId(), StandardCharsets.UTF_8.name()),
-                        UriUtils.encode("{\"auto.offset.reset\":\"latest\"}", StandardCharsets.UTF_8.name())));
+                        UriUtils.encode("{\"auto.offset.reset\":\"latest\"}", StandardCharsets.UTF_8.name())),
+                "title");
     }
     private UseCase getFilterUseCase() throws UnsupportedEncodingException {
         String cmd = "CREATE STREAM IF NOT EXISTS PAGEVIEWS_FILTERED_USER_1\n    with (kafka_topic='pageviews_filtered_user_1')\n    AS SELECT * FROM PAGEVIEWS WHERE USERID = 'User_1';";
         return new UseCase(
-                "Filter an event stream",
+                "description",
                 "pageviews_filtered_user_1",
                 "pageviews",
                 cmd,
@@ -49,12 +50,13 @@ public class UseCasesController {
                         urlService.getKsqlDbUrl(), // &ksqlClusterId=lksqlc-1v2p6&properties=%7B%22auto.offset.reset%22%3A%22latest%22%7D
                         UriUtils.encode(cmd, StandardCharsets.UTF_8.name()),
                         UriUtils.encode(urlService.getKsqlDbId(), StandardCharsets.UTF_8.name()),
-                        UriUtils.encode("{\"auto.offset.reset\":\"latest\"}", StandardCharsets.UTF_8.name())));
+                        UriUtils.encode("{\"auto.offset.reset\":\"latest\"}", StandardCharsets.UTF_8.name())),
+                "title");
     }
     private UseCase getAggregateUseCase() throws UnsupportedEncodingException {
         String cmd = "CREATE TABLE IF NOT EXISTS PAGEVIEWS_COUNT_BY_USER\n    with (kafka_topic='pageviews_count_by_user')\n    AS SELECT USERID, COUNT(*) AS numusers\n    FROM PAGEVIEWS WINDOW TUMBLING (size 30 second)\n    GROUP BY USERID HAVING COUNT(*) > 1;";
         return new UseCase(
-                "Aggregate an event stream",
+                "description",
                 "pageviews_count_by_user",
                 "pageviews",
                 cmd,
@@ -63,7 +65,8 @@ public class UseCasesController {
                         urlService.getKsqlDbUrl(), // &ksqlClusterId=lksqlc-1v2p6&properties=%7B%22auto.offset.reset%22%3A%22latest%22%7D
                         UriUtils.encode(cmd, StandardCharsets.UTF_8.name()),
                         UriUtils.encode(urlService.getKsqlDbId(), StandardCharsets.UTF_8.name()),
-                        UriUtils.encode("{\"auto.offset.reset\":\"latest\"}", StandardCharsets.UTF_8.name())));
+                        UriUtils.encode("{\"auto.offset.reset\":\"latest\"}", StandardCharsets.UTF_8.name())),
+                "title");
     }
     @GetMapping
     public List<UseCase> getUseCases() throws UnsupportedEncodingException {
