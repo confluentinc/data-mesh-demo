confluentinc/data-mesh-demo

commit b0359b9d6cc627f44dd4311e2c9edfd4b97ce453

UI: Improved display when there are network errors.
On some pages we weere showing the error multiple times, which seems
excessive. :-D

client/src/View/Create.elm
@@ -18,26 +18,32 @@ view activeUseCaseKey model =
     div [ class "create-pane" ]
         [ header []
             [ Markdown.toHtml [] dataProductCreationIntro ]
-        , div [ class "create-use-cases" ]
-            [ webDataView
-                (useCasesView activeUseCaseKey)
-                model.useCases
-            ]
-        , div [ class "create-use-detail" ]
-            [ webDataView
-                (\useCases ->
-                    let
-                        activeUseCase =
-                            Maybe.andThen (\k -> Dict.get unUseCaseName k useCases) activeUseCaseKey
-                    in
-                    useCasesDetail
-                        activeUseCase
-                        model.executeUseCaseResult
-                )
-                model.useCases
-            ]
+        , webDataView
+            (useCasesOverview
+                activeUseCaseKey
+                model.executeUseCaseResult
+            )
+            model.useCases
         , footer []
             [ Markdown.toHtml [] dataProductCreationOutro ]
+        ]
+
+
+useCasesOverview activeUseCaseKey executeUseCaseResult useCases =
+    let
+        activeUseCase =
+            Maybe.andThen (\k -> Dict.get unUseCaseName k useCases) activeUseCaseKey
+    in
+    div []
+        [ div [ class "create-use-cases" ]
+            [ useCasesView activeUseCaseKey
+                useCases
+            ]
+        , div [ class "create-use-detail" ]
+            [ useCasesDetail
+                activeUseCase
+                executeUseCaseResult
+            ]
         ]
 
 

client/src/View/Discover.elm
@@ -1,13 +1,13 @@
 module View.Discover exposing (deleteConfirmationDialog, view)
 
 import Dialog.Common as Dialog
-import GenericDict as Dict
+import GenericDict as Dict exposing (Dict)
 import Html exposing (..)
 import Html.Attributes exposing (class, disabled, href, target, type_, value)
 import Html.Events exposing (onClick)
 import Json.Extras as Json
 import Markdown
-import RemoteData exposing (RemoteData(..))
+import RemoteData exposing (RemoteData(..), WebData)
 import Table exposing (defaultCustomizations)
 import Table.Extras as Table
 import Types exposing (..)
@@ -20,48 +20,16 @@ import View.Tooltips exposing (tooltip)
 
 view : Maybe QualifiedName -> Model -> Html Msg
 view activeStreamKey model =
-    let
-        activeStream =
-            Maybe.map2
-                (Dict.get unQualifiedName)
-                activeStreamKey
-                (RemoteData.toMaybe model.streams)
-                |> Maybe.withDefault Nothing
-    in
     div [ class "discover-pane" ]
         [ header []
             [ Markdown.toHtml [] discoveryIntro ]
-        , div [ class "discover-main" ]
-            [ h2 []
-                [ text
-                    (case model.actuatorInfo of
-                        Success actuatorInfo ->
-                            "Data Products available (" ++ unDomain actuatorInfo.domain ++ " domain)"
-
-                        _ ->
-                            "Data Products available"
-                    )
-                , tooltip "Discover the data products that are publicly visible to your domain"
-                ]
-            , webDataView
-                (Table.view
-                    (tableConfig activeStreamKey)
-                    model.dataProductsTableState
-                )
-                (RemoteData.map
-                    (Dict.values >> filterDataProducts)
-                    model.streams
-                )
-            ]
-        , div [ class "discover-detail" ]
-            [ h2 []
-                [ text "Data Products Detail"
-                , tooltip "Contains all the relevant info about your data products. This information is intended to help you identify the data products relevant to your business use-case"
-                ]
-            , streamDetailView
-                (RemoteData.toMaybe model.actuatorInfo)
-                activeStream
-            ]
+        , webDataView
+            (dataProductsView
+                activeStreamKey
+                model.actuatorInfo
+                model.dataProductsTableState
+            )
+            model.streams
         , div [ class "discover-search" ]
             [ p []
                 (case ( model.actuatorInfo, Url.fromString "https://confluent.cloud/search" ) of
@@ -79,6 +47,53 @@ view activeStreamKey model =
             ]
         , footer []
             [ Markdown.toHtml [] discoveryOutro ]
+        ]
+
+
+dataProductsView :
+    Maybe QualifiedName
+    -> WebData ActuatorInfo
+    -> Table.State
+    -> Dict QualifiedName Stream
+    -> Html Msg
+dataProductsView activeStreamKey actuatorInfo dataProductsTableState streams =
+    let
+        activeStream =
+            activeStreamKey
+                |> Maybe.map
+                    (\key -> Dict.get unQualifiedName key streams)
+                |> Maybe.withDefault Nothing
+    in
+    div []
+        [ div [ class "discover-main" ]
+            [ h2 []
+                [ text
+                    (case actuatorInfo of
+                        Success info ->
+                            "Data Products available (" ++ unDomain info.domain ++ " domain)"
+
+                        _ ->
+                            "Data Products available"
+                    )
+                , tooltip "Discover the data products that are publicly visible to your domain"
+                ]
+            , Table.view
+                (tableConfig activeStreamKey)
+                dataProductsTableState
+                (streams
+                    |> Dict.values
+                    |> filterDataProducts
+                )
+            ]
+        , div [ class "discover-detail" ]
+            [ h2 []
+                [ text "Data Products Detail"
+                , tooltip "Contains all the relevant info about your data products. This information is intended to help you identify the data products relevant to your business use-case"
+                ]
+            , streamDetailView
+                (RemoteData.toMaybe actuatorInfo)
+                activeStream
+            ]
         ]
 
 

commit fee3777943b347cdd3d8c690d9e654b7096e21b4

UI: Fixing a layout problem caused by `b0359b9`.
My changes had made some grid elements become non-immediate children,
which broke the grid rules. :-(

client/src/View/Create.elm
@@ -15,36 +15,29 @@ import View.Tooltips exposing (tooltip)
 
 view : Maybe UseCaseName -> Model -> Html Msg
 view activeUseCaseKey model =
-    div [ class "create-pane" ]
-        [ header []
-            [ Markdown.toHtml [] dataProductCreationIntro ]
-        , webDataView
-            (useCasesOverview
-                activeUseCaseKey
-                model.executeUseCaseResult
-            )
-            model.useCases
-        , footer []
-            [ Markdown.toHtml [] dataProductCreationOutro ]
-        ]
-
-
-useCasesOverview activeUseCaseKey executeUseCaseResult useCases =
-    let
-        activeUseCase =
-            Maybe.andThen (\k -> Dict.get unUseCaseName k useCases) activeUseCaseKey
-    in
-    div []
-        [ div [ class "create-use-cases" ]
-            [ useCasesView activeUseCaseKey
-                useCases
-            ]
-        , div [ class "create-use-detail" ]
-            [ useCasesDetail
-                activeUseCase
-                executeUseCaseResult
-            ]
-        ]
+    webDataView
+        (\useCases ->
+            let
+                activeUseCase =
+                    Maybe.andThen (\k -> Dict.get unUseCaseName k useCases) activeUseCaseKey
+            in
+            div [ class "create-pane" ]
+                [ header []
+                    [ Markdown.toHtml [] dataProductCreationIntro ]
+                , div [ class "create-use-cases" ]
+                    [ useCasesView activeUseCaseKey
+                        useCases
+                    ]
+                , div [ class "create-use-detail" ]
+                    [ useCasesDetail
+                        activeUseCase
+                        model.executeUseCaseResult
+                    ]
+                , footer []
+                    [ Markdown.toHtml [] dataProductCreationOutro ]
+                ]
+        )
+        model.useCases
 
 
 dataProductCreationIntro : String

client/src/View/Discover.elm
@@ -20,81 +20,67 @@ import View.Tooltips exposing (tooltip)
 
 view : Maybe QualifiedName -> Model -> Html Msg
 view activeStreamKey model =
-    div [ class "discover-pane" ]
-        [ header []
-            [ Markdown.toHtml [] discoveryIntro ]
-        , webDataView
-            (dataProductsView
-                activeStreamKey
-                model.actuatorInfo
-                model.dataProductsTableState
-            )
-            model.streams
-        , div [ class "discover-search" ]
-            [ p []
-                (case ( model.actuatorInfo, Url.fromString "https://confluent.cloud/search" ) of
-                    ( Success actuatorInfo, Just url ) ->
-                        [ linkButton actuatorInfo.hostedMode
-                            ( "Advanced Search"
-                            , url
-                            , SearchScreenshot
+    webDataView
+        (\streams ->
+            let
+                activeStream =
+                    activeStreamKey
+                        |> Maybe.map
+                            (\key -> Dict.get unQualifiedName key streams)
+                        |> Maybe.withDefault Nothing
+            in
+            div [ class "discover-pane" ]
+                [ header []
+                    [ Markdown.toHtml [] discoveryIntro ]
+                , div [ class "discover-main" ]
+                    [ h2 []
+                        [ text
+                            (case model.actuatorInfo of
+                                Success info ->
+                                    "Data Products available (" ++ unDomain info.domain ++ " domain)"
+
+                                _ ->
+                                    "Data Products available"
                             )
+                        , tooltip "Discover the data products that are publicly visible to your domain"
                         ]
+                    , Table.view
+                        (tableConfig activeStreamKey)
+                        model.dataProductsTableState
+                        (streams
+                            |> Dict.values
+                            |> filterDataProducts
+                        )
+                    ]
+                , div [ class "discover-detail" ]
+                    [ h2 []
+                        [ text "Data Products Detail"
+                        , tooltip "Contains all the relevant info about your data products. This information is intended to help you identify the data products relevant to your business use-case"
+                        ]
+                    , streamDetailView
+                        (RemoteData.toMaybe model.actuatorInfo)
+                        activeStream
+                    ]
+                , div [ class "discover-search" ]
+                    [ p []
+                        (case ( model.actuatorInfo, Url.fromString "https://confluent.cloud/search" ) of
+                            ( Success actuatorInfo, Just url ) ->
+                                [ linkButton actuatorInfo.hostedMode
+                                    ( "Advanced Search"
+                                    , url
+                                    , SearchScreenshot
+                                    )
+                                ]
 
-                    _ ->
-                        []
-                )
-            ]
-        , footer []
-            [ Markdown.toHtml [] discoveryOutro ]
-        ]
-
-
-dataProductsView :
-    Maybe QualifiedName
-    -> WebData ActuatorInfo
-    -> Table.State
-    -> Dict QualifiedName Stream
-    -> Html Msg
-dataProductsView activeStreamKey actuatorInfo dataProductsTableState streams =
-    let
-        activeStream =
-            activeStreamKey
-                |> Maybe.map
-                    (\key -> Dict.get unQualifiedName key streams)
-                |> Maybe.withDefault Nothing
-    in
-    div []
-        [ div [ class "discover-main" ]
-            [ h2 []
-                [ text
-                    (case actuatorInfo of
-                        Success info ->
-                            "Data Products available (" ++ unDomain info.domain ++ " domain)"
-
-                        _ ->
-                            "Data Products available"
-                    )
-                , tooltip "Discover the data products that are publicly visible to your domain"
+                            _ ->
+                                []
+                        )
+                    ]
+                , footer []
+                    [ Markdown.toHtml [] discoveryOutro ]
                 ]
-            , Table.view
-                (tableConfig activeStreamKey)
-                dataProductsTableState
-                (streams
-                    |> Dict.values
-                    |> filterDataProducts
-                )
-            ]
-        , div [ class "discover-detail" ]
-            [ h2 []
-                [ text "Data Products Detail"
-                , tooltip "Contains all the relevant info about your data products. This information is intended to help you identify the data products relevant to your business use-case"
-                ]
-            , streamDetailView
-                (RemoteData.toMaybe actuatorInfo)
-                activeStream
-            ]
-        ]
+        )
+        model.streams
 
 
 discoveryIntro : String