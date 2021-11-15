e2c20657b7b5ae45ce2f23470ef2bb41651df7a3
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-11-15 15:03:30 -0600

    reset-data-mesh script now takes the config file as an argument (#59)

--- scripts/reset-data-mesh.sh
+++ scripts/reset-data-mesh.sh
@@ -4,7 +4,7 @@ function drop_ksql_component() {
   curl -s --http1.1 -XPOST "$KSQLDB_ENDPOINT/ksql" -H "Accept: application/json" -H "Content-Type: application/json" -d '{"ksql":"DROP '"$TYPE"' IF EXISTS '"$NAME"';","streamsProperties": {}}' -u "$KSQLDB_BASIC_AUTH_USER_INFO"
 }
 function drop_topic() {
-  ccloud kafka topic delete $NAME
+  ccloud kafka topic delete $NAME --environment $ENVIRONMENT_ID --cluster $CLUSTER_ID
 }
 function drop_schema() {
   ccloud schema-registry schema delete --subject $NAME --version all
@@ -15,6 +15,13 @@ function drop_data_product() {
   NAME=$NAME drop_topic
   NAME="${NAME}-value" drop_schema
 }
+
+CONFIG_FILE=$1
+
+KSQLDB_ENDPOINT=$( grep "^ksql.endpoint" $CONFIG_FILE | awk -F'=' '{print $2;}' )
+KSQLDB_BASIC_AUTH_USER_INFO=$( grep "^ksql.basic.auth.user.info" $CONFIG_FILE | awk -F'=' '{print $2;}' )
+ENVIRONMENT_ID=$( grep "^confluent.cloud.environment.id" $CONFIG_FILE | awk -F'=' '{print $2;}' )
+CLUSTER_ID=$( grep "^confluent.cloud.kafka.cluster.id" $CONFIG_FILE | awk -F'=' '{print $2;}' )
 
 # Delete any data products that might have been created by users
 NAME="us_enriched_stock_trades" TYPE="STREAM" drop_data_product
